8783a2569d9848fbe978679de04fbaea68cc7a93
陈靖雯 <916752309@qq.com>
2019-09-15 18:08:32 +0800

    import java project

--- src/partitionaddress/partition1.java
+++ src/partitionaddress/partition1.java
@@ -1,8 +1,5 @@
 package partitionaddress;
-
-import java.util.ArrayList;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
